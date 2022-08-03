e5ff2ee93ec8fb1b6a77113760ecf2ddcf7e5646
Jules Simon <jules.simon@protonmail.com>
2022-08-03 14:55:03 +0200

    First implementation of median on SO3

--- SO3/SO3.py
+++ SO3/SO3.py
@@ -1,29 +1,32 @@
 import numpy as np
 
+
 def skew(v):
-    return [
-        [0, -v[2], v[1]], 
-        [v[2], 0, -v[0]],
-        [-v[1], v[0], 0]]
+    return [[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]]
+
 
 def vee(W):
     return [-W[1][2], W[0][2], -W[0][1]]
 
-class SO3():
+
+class SO3:
     def __init__(self, R):
         self.R = R
 
     def __repr__(self):
         return f"{self.R}"
 
+    def __matmul__(self, rhs):
+        return SO3(self.R @ rhs.R)
+
     def log_map(self):
         """
-        Returns the skew matrix in lie algebra associated to the 
+        Returns the skew matrix in lie algebra associated to the
         SO3 rotation matrix R
         """
         R = self.R
         A = (R - R.T) / 2
-        norm = np.sqrt(-np.trace(A @ A)/2)
+        norm = np.sqrt(-np.trace(A @ A) / 2)
         skew_matrix = ((np.arcsin(norm)) / norm) * A
         return so3(vee(skew_matrix))
 
@@ -31,14 +34,13 @@ class SO3():
         """
         Returns the log map centered on other
         """
-        centered_SO3 = SO3(self.R @ self.R.T) # R@S^-1
+        centered_SO3 = SO3(self.R @ other.R.T)  # R@S^-1
         return centered_SO3.log_map()
 
 
-
-class so3():
+class so3:
     def __init__(self, w):
-        self.w = w # euler angles
+        self.w = w  # euler angles
 
     def __repr__(self):
         return f"{self.w}"
@@ -51,7 +53,7 @@ class so3():
         A = skew(self.w)
         exp = np.eye(3)
         for k in range(1, 50):
-            exp += (1/np.math.factorial(k)) * np.linalg.matrix_power(A, k)
+            exp += (1 / np.math.factorial(k)) * np.linalg.matrix_power(A, k)
         return SO3(exp)
 
     def exp_map_euler(self):
@@ -66,8 +68,20 @@ class so3():
         s = np.sin(theta / 2)
 
         exp = [
-            [2*(x**2 - 1)*s**2 + 1, 2*x*y*(s**2) - 2*z*c*s, 2*x*z*s**2 + 2*y*c*s],
-            [2*x*y*s**2 + 2*z*c*s, 2*(y**2-1)*s**2+1, 2*y*z*s**2 - 2*x*c*s],
-            [2*x*z*s**2 - 2*y*c*s, 2*y*z*s**2 + 2*x*c*s, 2*(z**2-1)*s**2 + 1]
+            [
+                2 * (x**2 - 1) * s**2 + 1,
+                2 * x * y * (s**2) - 2 * z * c * s,
+                2 * x * z * s**2 + 2 * y * c * s,
+            ],
+            [
+                2 * x * y * s**2 + 2 * z * c * s,
+                2 * (y**2 - 1) * s**2 + 1,
+                2 * y * z * s**2 - 2 * x * c * s,
+            ],
+            [
+                2 * x * z * s**2 - 2 * y * c * s,
+                2 * y * z * s**2 + 2 * x * c * s,
+                2 * (z**2 - 1) * s**2 + 1,
+            ],
         ]
         return SO3(exp)
