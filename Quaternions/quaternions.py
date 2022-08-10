7f5a2d18c127825700c05ab90f53e6a1450f00eb
Jules Simon <jules.simon@protonmail.com>
2022-08-10 10:05:11 +0200

    Extend the quaternion class

--- Quaternions/quaternions.py
+++ Quaternions/quaternions.py
@@ -9,7 +9,7 @@ class Quaternion:
         self.w = coeffs
 
     def __repr__(self):
-        return f"q = {self.a} + {self.b}i + {self.c}j + {self.d}k"
+        return f"q = {self.w[0]} + {self.w[1]}i + {self.w[2]}j + {self.w[3]}k"
 
     def scalar(self):
         return self.w[0]
@@ -21,4 +21,54 @@ class Quaternion:
         return sum(self.vector()) == 0  # TODO proper floating point
 
     def isVector(self):
-        return sum(self.scalar()) == 0
+        return self.scalar() == 0
+
+    def __add__(self, rhs):
+        """
+        Component wise addition
+        """
+        return Quaternion(
+            [
+                self.w[0] + rhs.w[0],
+                self.w[1] + rhs.w[1],
+                self.w[2] + rhs.w[2],
+                self.w[3] + rhs.w[3],
+            ]
+        )
+
+    def __mul__(self, rhs: "scalar"):
+        """
+        Component wise multiplication by a scalar
+        """
+        # TODO type checking
+        return Quaternion(
+            [
+                rhs * self.w[0],
+                rhs * self.w[1],
+                rhs * self.w[2],
+                rhs * self.w[3],
+            ]
+        )
+
+    def __matmul__(self, rhs):
+        """
+        Hamilton product
+        """
+        q1 = self.w
+        q2 = rhs.w
+        return Quaternion(
+            [
+                q1[0] * q2[0] - q1[1] * q2[1] - q1[2] * q2[2] - q1[3] * q2[3],
+                q1[0] * q2[1] + q1[1] * q2[0] + q1[2] * q2[3] - q1[3] * q2[2],
+                q1[0] * q2[2] - q1[1] * q2[3] + q1[2] * q2[0] + q1[3] * q2[1],
+                q1[0] * q2[3] + q1[1] * q2[2] - q1[2] * q2[1] + q1[3] * q2[0],
+            ]
+        )
+
+    def conjugate(self):
+        return Quaternion([self.w[0], -self.w[1], -self.w[2], -self.w[3]])
+
+    def inverse(self):
+        assert sum(self.w) != 0  # TODO FP comparison
+        factor = 1 / sum([x**2 for x in self.w])
+        return self.conjugate() * factor
