c2d3fa215e50205dc7d77a6e5067db56d6177818
Jules Simon <jules.simon@protonmail.com>
2022-09-09 10:23:37 +0200

    Unit test for line contains

--- 2DProjectiveGeometry/test/line.py
+++ 2DProjectiveGeometry/test/line.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 sys.path.append("..")
 
-from geometry import Line
+from geometry import Line, HomogeneousPoint
 
 
 class TestLine(unittest.TestCase):
@@ -12,6 +12,8 @@ class TestLine(unittest.TestCase):
         self.b0 = 1
         self.c0 = -5
         self.line1 = Line([self.a0, self.b0, self.c0])
+        self.point1 = HomogeneousPoint([1, 4, 1]) # TODO compute from line equation
+        self.point2 = HomogeneousPoint([0, 6, 1])
 
         
     def test_defaultConstructor(self):
@@ -22,8 +24,12 @@ class TestLine(unittest.TestCase):
         Use the line equation to get a point that is
         on the line
         """
-        point_on_line = 0
+        self.assertTrue(self.point1 in self.line1)
+        self.assertFalse(self.point2 in self.line1)
+
+    def test_intersection(self):
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
