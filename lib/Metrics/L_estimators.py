2a10c9cae9004490cf8efe86226718d2d3c4260e
Jules Simon <jules.simon@protonmail.com>
2023-01-18 15:36:17 +0100

    Add S and Q scale metrics

--- lib/Metrics/L_estimators.py
+++ lib/Metrics/L_estimators.py
@@ -1,4 +1,5 @@
 
+
 def median(data: list):
     assert len(data) > 0, "List is empty"
     if len(data) == 1:
@@ -27,6 +28,7 @@ def Q3(data: list):
     n = len(data) // 2
     return median(data[n:])
     
+# Robust estimates of scale
 def median_absolute_deviation(data: list):
     median_data = median(data)
     residuals_from_median = [abs(x_i - median_data) for x_i in data]
@@ -34,3 +36,38 @@ def median_absolute_deviation(data: list):
 
 def interquartile_range(data: list):
     return Q3(data) - Q1(data)
+
+def S(data: list):
+    """
+    For each i we compute the median of |x_i - x_j|,
+    where j = 1...n = len(data).
+    This yields n numbers, of which we compute the median.
+    """
+    pairwise_medians = []
+    for i in range(len(data)):
+        abs_diff_from_i = []
+        for j in range(len(data)):
+            abs_diff_from_i.append(abs(data[i] - data[j]))
+        pairwise_medians.append(median(abs_diff_from_i))
+    return 1.1926 * median(pairwise_medians)
+
+def Q(data: list):
+    """
+    First quartile of pairwise differences
+    """
+    abs_diff_from_i = []
+    for i in range(len(data)):
+        for j in range(i, len(data)):
+            abs_diff_from_i.append(abs(data[i] - data[j]))
+    return 2.2219 * Q1(abs_diff_from_i)
+
+
+def is_outlier(data: list, data_point):
+    """
+    Using the MAD, check is data_point is an outlier
+    """
+    mad = median_absolute_deviation(data)
+    med = median(data)
+    threshold = 2.5
+    criteria = abs(data_point - med) / mad
+    return criteria > threshold
