aef2880e751f2585ed209841a0b106e520904128
Jules Simon <jules.simon@protonmail.com>
2023-03-02 06:58:25 +0100

    Fix matrix test

--- lib/LinearAlgebra/tests/test_matrix.py
+++ lib/LinearAlgebra/tests/test_matrix.py
@@ -1,25 +1,25 @@
 import unittest
-from lib.LinearAlgebra import matrix
+from lib.LinearAlgebra.matrix import transpose
 from random import random
 import numpy as np
 
-class TestMatrix(unittest.TestCase):
 
+class TestMatrix(unittest.TestCase):
     def setUp(self):
         self.identity = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 
     def test_constructor(self):
-        self.assertTrue(np.allclose(self.identity, matrix.transpose(self.identity)))
+        self.assertTrue(np.allclose(self.identity, transpose(self.identity)))
 
     def test_with_random(self):
         matrix = []
         for i in range(3):
-            matrix[i] = [random(), random(), random()]
-        
-        matrix_transpose = matrix.transpose(matrix)
-        matrix_transpose_transpose = matrix.transpose(matrix_transpose)
+            matrix.append([random(), random(), random()])
+
+        matrix_transpose = transpose(matrix)
+        matrix_transpose_transpose = transpose(matrix_transpose)
         self.assertTrue(np.allclose(matrix_transpose_transpose, matrix))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
