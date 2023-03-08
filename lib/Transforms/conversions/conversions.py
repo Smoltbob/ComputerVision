0f15abfb04fc239fea046f871536c618ecabc2ef
Jules Simon <jules.simon@protonmail.com>
2023-03-08 07:26:19 +0100

    Add tests for SO3/S3 conversionts

--- lib/Transforms/conversions/conversions.py
+++ lib/Transforms/conversions/conversions.py
@@ -2,11 +2,13 @@ from lib.Transforms.s3.s3 import S3
 from lib.Transforms.SO3.SO3 import SO3
 
 
-def S3_from_SO3(SO3_matrix):
+def S3_from_SO3(SO3):
     """
     Matrix to unit quaternion conversion
+    see https://d3cw3dd2w32x2b.cloudfront.net/wp-content/uploads/2015/01/matrix-to-quat.pdf
     """
-    # see https://d3cw3dd2w32x2b.cloudfront.net/wp-content/uploads/2015/01/matrix-to-quat.pdf
+    SO3_matrix = SO3.R
+
     r00 = SO3_matrix[0][0]
     r01 = SO3_matrix[0][1]
     r02 = SO3_matrix[0][2]
@@ -51,16 +53,24 @@ def SO3_from_S3(S3_quat):
     """
     Unit quaternion to matrix conversion
     """
-    qr, qi, qj, qk = S3_quat.w
-    r00 = 1 - 2 * (qj**2 + qk**2)
-    r01 = 2 * (qi * qj - qk * qr)
-    r02 = 2 * (qi * qk + qj * qr)
-    r10 = 2 * (qi * qj + qk * qr)
-    r11 = 1 - 2 * (qi**2 + qk**2)
-    r12 = 2 * (qj * qk - qi * qr)
-    r20 = 2 * (qi * qk - qj * qr)
-    r21 = 2 * (qj * qk + qi * qr)
-    r22 = 1 - 2 * (qi**2 + qj**2)
 
-    R = [[r00, r01, r02], [r10, r11, r12], [r20, r21, r22]]
+    w, x, y, z = S3_quat.w
+
+    txx = 2 * x * x
+    tyy = 2 * y * y
+    tzz = 2 * z * z
+    tzz = 2 * z * z
+    txy = 2 * x * y
+    txz = 2 * x * z
+    tyz = 2 * y * z
+    txw = 2 * x * w
+    tyw = 2 * y * w
+    tzw = 2 * z * w
+
+    R = [
+        [1 - (tyy + tzz), (txy - tzw), (txz + tyw)],
+        [(txy + tzw), 1 - (txx + tzz), (tyz - txw)],
+        [(txz - tyw), (tyz + txw), 1 - (txx + tyy)],
+    ]
+
     return SO3(R)
--- lib/Transforms/conversions/tests/test_conversions.py
+++ lib/Transforms/conversions/tests/test_conversions.py
@@ -1,7 +1,7 @@
 import unittest
 from lib.Transforms.SO3.SO3 import SO3
 from lib.Transforms.s3.s3 import S3
-from lib.Transforms.conversions.conversions import SO3_from_S3
+from lib.Transforms.conversions.conversions import SO3_from_S3, S3_from_SO3
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
@@ -11,9 +11,25 @@ class TestConversions(unittest.TestCase):
         self.identity_SO3 = SO3([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
         self.identity_S3 = S3([1, 0, 0, 0])
 
-    def test_identity_transforms(self):
+    def test_SO3_from_S3_identity_transforms(self):
         converted_SO3_from_S3 = SO3_from_S3(self.identity_S3)
         self.assertTrue(np.allclose(converted_SO3_from_S3.R, self.identity_SO3.R))
+
+    def test_SO3_from_S3_and_back_random_transforms(self):
+        for _ in range(100):
+            random_q = S3(np.random.rand(4))
+            converted_SO3_from_S3 = SO3_from_S3(random_q)
+            converted_S3_from_SO3 = S3_from_SO3(converted_SO3_from_S3)
+
+            self.assertTrue(np.allclose(random_q.w, converted_S3_from_SO3.w))
+
+    def test_S3_from_S03_and_back_random_transforms(self):
+        for _ in range(100):
+            random_R = SO3(R.random().as_matrix())
+            converted_S3_from_SO3 = S3_from_SO3(random_R)
+            converted_SO3_from_S3 = SO3_from_S3(converted_S3_from_SO3)
+
+            self.assertTrue(np.allclose(random_R.R, converted_SO3_from_S3.R))
 
 
 if __name__ == "__main__":
