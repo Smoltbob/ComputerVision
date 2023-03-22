2d32ee23432a525599a2cae1007511e10133c7cd
Jules Simon <jules.simon@protonmail.com>
2023-03-22 08:26:36 +0100

    Add tests for transpose

--- lib/LinearAlgebra/matrix.cpp
+++ lib/LinearAlgebra/matrix.cpp
@@ -5,32 +5,34 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-
-auto transpose(std::vector<std::vector<double>>const& matrix) -> std::vector<std::vector<double>> { 
-   assert(matrix.size() > 2);
-   std::vector<std::vector<double>> output{};
-   for (auto const& row: matrix)
-   {
+auto transpose(std::vector<std::vector<double>> const &matrix) -> std::vector<std::vector<double>>
+{
+    assert(matrix.size() > 2);
+    std::vector<std::vector<double>> output{};
+    for (auto const &row : matrix)
+    {
         assert(matrix.size() == row.size());
-   }
-  
-   for (std::size_t i = 0; i < matrix.size(); ++i)
-   {
+    }
+
+    for (std::size_t i = 0; i < matrix.size(); ++i)
+    {
         std::vector<double> new_row{};
         for (std::size_t j = 0; j < matrix.size(); ++j)
         {
             new_row.push_back(matrix.at(j).at(i));
         }
         output.push_back(new_row);
-
-   }
-   return output;
+    }
+    return output;
 }
 
-auto compute_determinant_3x3() -> double{
+auto compute_determinant_3x3() -> double
+{
+
     return 0;
 }
 
-PYBIND11_MODULE(matrix, m) {
+PYBIND11_MODULE(matrix, m)
+{
     m.def("transpose", &transpose, "Transpose a square matrix");
 }
