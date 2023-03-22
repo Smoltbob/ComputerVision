37831568be0f0d0b1736b02788590f5d06561a2b
Jules Simon <jules.simon@protonmail.com>
2023-03-22 08:27:02 +0100

    Add tests for transpose

--- lib/LinearAlgebra/tests/test_matrix.py
+++ lib/LinearAlgebra/tests/test_matrix.py
@@ -1,5 +1,5 @@
 import unittest
-from lib.LinearAlgebra.matrix import transpose
+from lib.LinearAlgebra.matrix import transpose, compute_determinant_3x3
 from random import random
 import numpy as np
 
@@ -11,14 +11,37 @@ class TestMatrix(unittest.TestCase):
     def test_constructor(self):
         self.assertTrue(np.allclose(self.identity, transpose(self.identity)))
 
-    def test_with_random(self):
+    def test_transpose_random(self):
         matrix = []
         for i in range(3):
             matrix.append([random(), random(), random()])
 
+        # Transpose of transpose is equal to original
         matrix_transpose = transpose(matrix)
         matrix_transpose_transpose = transpose(matrix_transpose)
         self.assertTrue(np.allclose(matrix_transpose_transpose, matrix))
+
+        # Determinant of transpose is equal to determinant of original
+        self.assertTrue(
+            np.allclose(
+                compute_determinant_3x3(matrix_transpose),
+                compute_determinant_3x3(matrix),
+            )
+        )
+
+        # (AB)^T == B^TA^T
+        A = []
+        B = []
+        for i in range(3):
+            A.append([random(), random(), random()])
+            B.append([random(), random(), random()])
+
+        A = np.array(A)
+        B = np.array(B)
+        AB_t = transpose(A @ B)
+        B_tA_t = np.array(transpose(B)) @ np.array(transpose(A))
+
+        self.assertTrue(np.allclose(AB_t, B_tA_t)
 
 
 if __name__ == "__main__":
