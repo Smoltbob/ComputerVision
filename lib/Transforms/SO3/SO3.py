a206ccf73190ddaaaa3472e4f117cf14a9b92dcc
Jules Simon <jules.simon@protonmail.com>
2023-04-01 16:05:16 +0200

    Fix SO3 median notebook

--- lib/Transforms/SO3/SO3.py
+++ lib/Transforms/SO3/SO3.py
@@ -82,35 +82,6 @@ class SO3:
     return centered_SO3.log_map()
     """
 
-    @classmethod
-    def SO3_median(rotation_list: list, threshold=1e-6, S_t=None):
-        # We provide S_t as a first guess
-        assert S_t is not None
-
-        for _ in range(100):
-            elements = np.array(
-                [x.centered_log_map(S_t).w for x in rotation_list]
-            )
-            numerator = 0
-            denumerator = 0
-            for xi in elements:
-                X = np.linalg.norm(xi)
-                numerator += xi / X
-                denumerator += 1 / X
-            delta = numerator / denumerator
-            S_t = SO3.so3(delta).exp_map_euler() @ S_t
-
-            if np.linalg.norm(delta) < threshold:
-                break
-
-        return S_t
-
-    @classmethod
-    def SO3_mean(rotations):
-        # Correct ?
-        v_is = np.array([x.log_map().w for x in rotations])
-        return SO3.so3(np.mean(v_is, axis=0)).exp_map_euler()
-
     @staticmethod
     def isValid(rotation):
         # Checks that the rotation matrix is valid
---
+++ lib/Transforms/SO3/SO3_Utils.py
@@ -0,0 +1,31 @@
+from lib.Transforms.SO3 import SO3
+from lib.Transforms.so3 import so3
+from lib.Transforms.conversions.conversions import SO3_to_log, log_to_SO3
+import numpy as np
+
+
+def SO3_median(rotation_list: list, threshold=1e-6, S_t=None):
+    # We provide S_t as a first guess
+    assert S_t is not None
+
+    for _ in range(100):
+        elements = np.array([x.centered_log_map(S_t).w for x in rotation_list])
+        numerator = 0
+        denumerator = 0
+        for xi in elements:
+            X = np.linalg.norm(xi)
+            numerator += xi / X
+            denumerator += 1 / X
+        delta = numerator / denumerator
+        S_t = SO3.so3(delta).exp_map_euler() @ S_t
+
+        if np.linalg.norm(delta) < threshold:
+            break
+
+    return S_t
+
+
+def SO3_mean(rotations):
+    # Correct ?
+    v_is = np.array([SO3_to_log(x).w for x in rotations])
+    return log_to_SO3(so3(np.mean(v_is, axis=0)))
