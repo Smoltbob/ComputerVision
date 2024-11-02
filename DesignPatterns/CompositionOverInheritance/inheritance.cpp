93c0e9c97a327be0c931bb6d7c7514a55c0f20c5
Jules Simon <jules.simon@protonmail.com>
2024-11-02 07:15:46 +0000

    Add example for inheritance

--- DesignPatterns/CompositionOverInheritance/inheritance.cpp
+++ DesignPatterns/CompositionOverInheritance/inheritance.cpp
@@ -9,7 +9,7 @@ class Object
         virtual void collide(Object objects[]) {}
 };
 
-class Visible : public Object
+class Visible : virtual public Object
 {
     public:
         virtual void draw() override
@@ -19,7 +19,7 @@ class Visible : public Object
         }
 };
 
-class Solid : public Object
+class Solid : virtual public Object
 {
     public:
         virtual void collide(Object objects[]) override
@@ -28,7 +28,7 @@ class Solid : public Object
         }
 };
 
-class Movable : public Object
+class Movable : virtual public Object
 {
     public:
         virtual void update() override
@@ -38,9 +38,47 @@ class Movable : public Object
 };
 
 // Using the properties to define behaviors in new classes
+// We need virtual inheritance to avoid creating multiple copies of the base class.
+class Player: virtual public Solid, virtual public Movable, virtual public Visible
+{};
 
+// The cloud is not solid, implicitly through the base Object class
+class Cloud: virtual public Movable, virtual public Visible
+{};
+
+// The building is  not movable, implicitly through the base Object class
+class Building: virtual public Solid, virtual public Visible
+{};
+
+// The trap ie neither visible or movable
+class Trap: virtual public Solid
+{};
 
 int main ()
 {
     std::cout << "Running the inheritance demo program" << std::endl;
+
+    std::cout << "Instantiating player:" << std::endl;
+    Player my_player{};
+    my_player.update();
+    my_player.collide(&my_player);
+    my_player.draw();
+
+    std::cout << "Instantiating cloud:" << std::endl;
+    Cloud my_cloud{};
+    my_cloud.update();
+    my_cloud.collide(&my_player); // There will be no collision
+    my_cloud.draw();
+
+    std::cout << "Instantiating building:" << std::endl;
+    Building my_building{};
+    my_building.update(); // There will be no move
+    my_building.collide(&my_player);
+    my_building.draw();
+
+    std::cout << "Instantiating trap:" << std::endl;
+    Trap my_trap{};
+    my_trap.update(); // There will be no move
+    my_trap.collide(&my_player);
+    my_trap.draw(); // There will be no draw
 }
