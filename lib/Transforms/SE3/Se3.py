32ae2aa30e86113433952ca77e41cc89f13f065d
Jules Simon <jules.simon@protonmail.com>
2026-01-05 13:43:53 +0100

    Add axis angle and translation getters to Se3

--- lib/Transforms/SE3/Se3.py
+++ lib/Transforms/SE3/Se3.py
@@ -1,5 +1,6 @@
 import numpy as np
 from lib.Math.rotation_utils import euler_to_rot_mat
+from typing import List
 
 class Se3:
     def __init__(self, translation: np.ndarray, rotation: np.ndarray):
@@ -25,6 +26,25 @@ class Se3:
         mat[:3, 3] = self.translation
         return mat
     
+    def getAxisAngle(self) -> List[float]:
+        """
+        Converts the rotation matrix to axis-angle representation.
+        Returns a list of 3 floats representing the rotation vector.
+        """
+        angle = np.arccos((np.trace(self.rotation) - 1) / 2)
+        if np.isclose(angle, 0):
+            return np.array([0.0, 0.0, 0.0])
+        rx = self.rotation[2, 1] - self.rotation[1, 2]
+        ry = self.rotation[0, 2] - self.rotation[2, 0]
+        rz = self.rotation[1, 0] - self.rotation[0, 1]
+        r = np.array([rx, ry, rz])
+        r = (angle / (2 * np.sin(angle))) * r
+        return r
+    
+    def getTranslation(self) -> List[float]:
+        """Returns the translation vector as a list of 3 floats."""
+        return self.translation
+    
     def transformPoint(self, point: np.ndarray) -> np.ndarray:
         """
         Transforms a 3D point using the SE3 transformation,
@@ -33,7 +53,8 @@ class Se3:
         Args:
             point: An homogeneous 3D point as a numpy array of shape (4,)
         """
-        return np.array([self.rotation @ point[:3] + point[3] * self.translation, point[3]]))
+        point1 = self.rotation @ point[:3] + point[3] * self.translation
+        return np.array([*point1, point[3]])
 
     @staticmethod
     def from_euler_angles(translation: np.ndarray, roll: float, pitch: float, yaw: float) -> "Se3":
